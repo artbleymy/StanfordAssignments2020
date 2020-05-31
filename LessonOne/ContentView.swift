305e07420bb1e5c45dd43cdfe269024653956c34
Stanislav Kozlov <bleymy@mail.ru>
2020-05-31 16:44:37 +0300

    Lesson one is finished

--- LessonOne/ContentView.swift
+++ LessonOne/ContentView.swift
@@ -11,7 +11,39 @@ import SwiftUI
 struct ContentView: View
 {
 	var body: some View {
-		Text("Hello, World!")
+		VStack {
+			ForEach(0..<4) { _ in
+				HStack {
+					ForEach(0..<4) {index in
+						CardView(isFaceUp: index % 2 == 0)
+					}
+				}
+			}
+		}
+			.padding()
+			.foregroundColor(Color.orange)
+			.font(Font.largeTitle)
+	}
+}
+
+struct CardView: View
+{
+	var isFaceUp: Bool
+
+	var body: some View {
+		ZStack {
+			if isFaceUp {
+				RoundedRectangle(cornerRadius: 10.0)
+					.fill(Color.white)
+				RoundedRectangle(cornerRadius: 10.0)
+					.stroke(lineWidth: 3)
+				Text("👻")
+			}
+			else {
+				RoundedRectangle(cornerRadius: 10.0)
+				.fill()
+			}
+		}
 	}
 }
 
